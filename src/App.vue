4e60865aeb824255020ffec8447086c538c5c1ea
yshKlee <timeisfault@qq.com>
2019-03-25 14:57:44 +0800

    add vue-cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo"
+         src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import TestD3 from './components/TestD3.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TestD3
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TestD3.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template> 
+<script>
+export default {
+  name: 'TestD3',
+  data () {
+    return {
+      asd: 'true'
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
